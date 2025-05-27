725ddda8786c14dfb77d6ae6a968d9a8da7f2800
God'swill Sekav Apya <godswillapya@gmail.com>
2025-05-27 22:43:47 +0100

    refactor operators.js: comment out unused code and maintain random number generation

--- operators.js
+++ operators.js
@@ -5,4 +5,26 @@
 // console.log(`The sum of ${sum1} and ${sum2} is ${sum}`);
 
 
-console.log(2 % 3);
+// console.log(2 % 3);
+
+
+// let a = 5;
+// console.log(a /= 3);
+// console.log(a);
+
+// a += 3; // a = a + 3
+// console.log(`After +=, a = ${a}`);
+
+// a -= 2; // a = a - 2
+// console.log(`After -=, a = ${a}`);
+
+// a *= 4; // a = a * 4
+// console.log(`After *=, a = ${a}`);
+
+// a /= 2; // a = a / 2
+// console.log(`After /=, a = ${a}`);
+
+// a %= 3; // a = a % 3
+// console.log(`After %=, a = ${a}`);
+
+console.log(Math.floor(Math.random()*100));
