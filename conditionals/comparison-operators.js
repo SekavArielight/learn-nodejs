8daa641276230c77de7849ecce4ac25c437b3501
God'swill Sekav Apya <godswillapya@gmail.com>
2025-05-28 23:25:55 +0100

    add conditionals.js: restore withdrawal logic with balance check

---
+++ conditionals/comparison-operators.js
@@ -0,0 +1,7 @@
+if (1 !== 3){
+    console.log("1 is equal to 3");
+}
+
+
+let lateToWork = true;
+console.log(!lateToWork)
---
+++ conditionals/conditionals-two.js
@@ -0,0 +1,28 @@
+const score = 55;
+
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+console.log(`Grade: ${grade}`);
+
+const size = 10;
+
+if (size > 100) {
+  console.log('Big');
+} else if (size > 20) {
+  console.log('Medium');
+} else if (size > 4) {
+  console.log('Small');
+} else {
+  console.log('Tiny');
+}
+// Print: Small
