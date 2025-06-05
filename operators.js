15dd0fad584341c320b61bbd50994460703e2ddf
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-05 01:35:16 +0100

    refactor operators.js: uncomment arithmetic operations and related console logs

--- operators.js
+++ operators.js
@@ -1,30 +1,30 @@
-// const sum1 = 2;
-// const sum2 = 3;
+const sum1 = 2;
+const sum2 = 3;
 
-// const sum = sum1 + sum2;
-// console.log(`The sum of ${sum1} and ${sum2} is ${sum}`);
-
-
-// console.log(2 % 3);
+const sum = sum1 + sum2;
+console.log(`The sum of ${sum1} and ${sum2} is ${sum}`);
 
 
-// let a = 5;
-// console.log(a /= 3);
-// console.log(a);
+console.log(2 % 3);
 
-// a += 3; // a = a + 3
-// console.log(`After +=, a = ${a}`);
 
-// a -= 2; // a = a - 2
-// console.log(`After -=, a = ${a}`);
+let a = 5;
+console.log(a /= 3);
+console.log(a);
 
-// a *= 4; // a = a * 4
-// console.log(`After *=, a = ${a}`);
+a += 3; // a = a + 3
+console.log(`After +=, a = ${a}`);
 
-// a /= 2; // a = a / 2
-// console.log(`After /=, a = ${a}`);
+a -= 2; // a = a - 2
+console.log(`After -=, a = ${a}`);
 
-// a %= 3; // a = a % 3
-// console.log(`After %=, a = ${a}`);
+a *= 4; // a = a * 4
+console.log(`After *=, a = ${a}`);
+
+a /= 2; // a = a / 2
+console.log(`After /=, a = ${a}`);
+
+a %= 3; // a = a % 3
+console.log(`After %=, a = ${a}`);
 
 console.log(Math.floor(Math.random()*100));
