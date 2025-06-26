e621a6af2a7f13951bbf217c0e8b1c91578556b0
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-26 09:25:16 +0100

    add loops.js and secondLoop.js: implement examples of do-while and while loops

---
+++ loops.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i <= 4; i++){
+//     console.log(i);
+// }
+
+x = 0
+i = 0
+
+do {
+  x = x + i;
+  console.log(x)
+  i++;
+} while (i < 5);
+
+// Prints: 0 1 3 6 10
+
+
+
+
+
+// while (condition) {
+  // code block to be executed
+// }
+
+let i = 0;
+
+while (i < 5) {        
+  console.log(i);
+  i++;
+}
