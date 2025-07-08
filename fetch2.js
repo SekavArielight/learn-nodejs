ca865339dff1882556c3f95197699d7a4d815d58
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-08 17:15:33 +0100

    add fetch2.js: implement data fetching with error handling and add .env to .gitignore

--- fetch2.js
+++ fetch2.js
@@ -1,1 +1,5 @@
 // fetch('https://dummyjson.com/recipes')
+fetch(`${process.env.API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
