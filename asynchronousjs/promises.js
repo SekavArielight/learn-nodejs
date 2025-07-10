18c55b30959c2471eff6329a5ea60d85bf63fcf1
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-10 15:10:14 +0100

    refactor: update promise variable naming and improve resolve/reject messages

--- asynchronousjs/promises.js
+++ asynchronousjs/promises.js
@@ -1,13 +1,41 @@
-const promise = new Promise ((resolve, reject) => {
+let promiseVar = new Promise((resolve, reject) => {
     let condition = true;
-    if (condition) {
-        resolve('Yey! We are happy');
+
+    if(condition){
+        resolve("Happy!!!!")
     } else {
-        reject('We have an error!');
+        reject("Somehow Happy!!!")
     }
 });
 
-promise.then(
+promiseVar.then(
     (result) => console.log(result),
     (error) => console.log(error)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const promise = new Promise ((resolve, reject) => {
+//     let condition = true;
+//     if (condition) {
+//         resolve('Yey! We are happy');
+//     } else {
+//         reject('We have an error!');
+//     }
+// });
+
+// promise.then(
+//     (result) => console.log(result),
+//     (error) => console.log(error)
+// );
