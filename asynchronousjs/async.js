c71c941edeb340d0c97d7bfed5db04f46d4225dd
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-10 14:54:35 +0100

    refactor: reorganize code structure and add commented console logs for clarity

--- asynchronousjs/async.js
+++ asynchronousjs/async.js
@@ -6,3 +6,15 @@
 //                               flow and allows the program to continue.
 //                               (I/O operations, network requests, fetching data)
 //                               Handled with: Callbacks, Promises, Async/Await
+
+// console.log('1')
+// console.log('2')
+// console.log('3')
+
+function greet (){
+    console.log("Hi");
+}
+
+console.log('1')
+setTimeout(greet, 5000)
+console.log('3')
